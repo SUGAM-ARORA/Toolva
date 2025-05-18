36f66bd93dea45fa57cced2a4f1897f6473d7c5f
Majin-Buu2314 <majin.buudbj@gmail.com>
2025-05-18 17:11:37 +0530

    Add default export to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-// Update the navigation in App.tsx
 import { Link } from 'react-router-dom';
 
-// Inside the header section, update the logo to be a Link
-<Link to="/" className="text-xl font-bold text-gray-900 dark:text-white ml-2">
-  Toolva
-</Link>
+function App() {
+  return (
+    <header>
+      <Link to="/" className="text-xl font-bold text-gray-900 dark:text-white ml-2">
+        Toolva
+      </Link>
+    </header>
+  );
+}
+
+export default App;
