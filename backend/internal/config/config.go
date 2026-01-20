838c56ecc4a72f0ef6d50b390f032e0764bf1389
avdhut400 <avdhutmagar@gmail.com>
2026-01-21 03:15:19 +0530

    Fix: align Favorite model UUID types with Supabase schema

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -1,40 +1,3 @@
-// package config
-
-// import (
-// 	"os"
-
-// 	"github.com/joho/godotenv"
-// )
-
-// type Config struct {
-// 	DBHost     string
-// 	DBPort     string
-// 	DBUser     string
-// 	DBPassword string
-// 	DBName     string
-// 	Port       string
-// }
-
-// func LoadConfig() (*Config, error) {
-// 	err := godotenv.Load()
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	return &Config{
-// 		DBHost:     os.Getenv("DB_HOST"),
-// 		DBPort:     os.Getenv("DB_PORT"),
-// 		DBUser:     os.Getenv("DB_USER"),
-// 		DBPassword: os.Getenv("DB_PASSWORD"),
-// 		DBName:     os.Getenv("DB_NAME"),
-// 		Port:       os.Getenv("PORT"),
-// 	}, nil
-// }
-
-
-
-
-
 
 package config
 
@@ -55,7 +18,7 @@ type Config struct {
 }
 
 func LoadConfig() (*Config, error) {
-	_ = godotenv.Load() // .env optional (error ignore)
+	_ = godotenv.Load() 
 
 	return &Config{
 		DBHost:     os.Getenv("DB_HOST"),
